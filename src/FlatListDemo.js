ddb8b7986af0b343dd30e6894f7a9211406e7b51
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-16 13:27:32 -0500

    added new list

--- src/FlatListDemo.js
+++ src/FlatListDemo.js
@@ -3,7 +3,23 @@ import { FlatList, Text, View } from 'react-native'
 const FlatListDemo = () =>  {
 const names=[
   {
-
+    name:"Ekram",
+  },
+    {
+  name:"Haque",
+  },
+    {
+  name:"Prema",
+  },
+    {
+  name:"Tasnim",
+  },
+    {
+  name:"Mahmudul",
+  },
+    {
+  name:"Mahin",
+  },
 ]
     return (
       <FlatList data={names} renderItem={(element)=>{
