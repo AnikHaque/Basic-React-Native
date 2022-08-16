604995a033060ff8c4e79953afeb529c85436b14
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-16 13:30:47 -0500

    updated lists again

--- src/FlatListDemo.js
+++ src/FlatListDemo.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import React from 'react'
+import { FlatList, Text,  } from 'react-native'
 const FlatListDemo = () =>  {
 const names=[
   {
