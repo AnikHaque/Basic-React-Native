8fe8c75e3196efbc548e7ae7fff73d3acf4e5e04
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-16 12:39:38 -0500

    added flatlist component

---
+++ src/FlatListDemo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+const FlatListDemo = () =>  {
+
+    return (
+      <View>
+        <Text>FlatListDemo</Text>
+      </View>
+    )
+
+}
