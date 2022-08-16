d511b2dfa2c250838c8cba5f4add3fecf5677418
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-16 13:27:07 -0500

    deleted all lists

--- src/FlatListDemo.js
+++ src/FlatListDemo.js
@@ -3,23 +3,7 @@ import { FlatList, Text, View } from 'react-native'
 const FlatListDemo = () =>  {
 const names=[
   {
-name:"Ekram",
-},
-  {
-name:"Haque",
-},
-  {
-name:"Prema",
-},
-  {
-name:"Tasnim",
-},
-  {
-name:"Mahmudul",
-},
-  {
-name:"Mahin",
-},
+
 ]
     return (
       <FlatList data={names} renderItem={(element)=>{
