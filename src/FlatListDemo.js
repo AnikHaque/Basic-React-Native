ff7fa27ac70fa1cf139d5cabe1fc9e623ffc8b50
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-18 13:09:28 -0500

    added

--- src/FlatListDemo.js
+++ src/FlatListDemo.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { FlatList, Text,  } from 'react-native'
 const FlatListDemo = () =>  {
 const names=[
-
+ 
+    {
+  name:"Haque",
+  },
     {
   name:"Anik",
   },
